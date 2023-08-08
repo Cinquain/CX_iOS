a3f539a3c648d80017309743184a96474c7e6277
James Allan <James@cityXcape.com>
2023-08-08 11:20:32 -0500

    Initial Commit

---
+++ CityXcape/CityXcapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityXcapeApp.swift
+//  CityXcape
+//
+//  Created by James Allan on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct CityXcapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
